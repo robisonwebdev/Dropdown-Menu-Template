e1bd786398e2b017463ec8ceb0ab2c80108dde0d
David Robison <david@robisonweb.dev>
2021-04-07 15:11:56 -0600

    Added window eventlistener to listen for click and remove show class

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,3 +1,15 @@
-function toggleDropdown(target) {
-  document.querySelector(target).classList.toggle("show");
+function toggleDropdown(value) {
+  const target = document.querySelector(value);
+
+  target.classList.toggle("show");
 }
+
+window.addEventListener("click", (event) => {
+  const show = document.querySelectorAll(".show");
+
+  if (!event.target.matches(".dropdownBtn")) {
+    for (let i = 0; i < show.length; i++) {
+      show[i].classList.remove("show");
+    }
+  }
+});
